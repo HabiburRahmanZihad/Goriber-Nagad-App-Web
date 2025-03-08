68cf3836780850cc6bb723ab17c7cab8a0020815
HabiburRahmanZihad <e241024@ugrad.iiuc.ac.bd>
2025-03-08 22:53:52 +0600

    Work in progress also pause this project for some week....

--- scripts/sendMoney.js
+++ scripts/sendMoney.js
@@ -3,3 +3,34 @@ sendMoneyCard.addEventListener('click', function (event) {
     event.preventDefault();
     window.location.href = 'sendMoney.html';
 });
+
+let cashOutCard = document.getElementById('cashOut-card');
+cashOutCard.addEventListener('click', function (event) {
+    event.preventDefault();
+    alert('Developer says: This feature is not available yet');
+});
+
+
+let mobileRechargeCard = document.getElementById('mobileRecharge-card');
+mobileRechargeCard.addEventListener('click', function (event) {
+    event.preventDefault();
+    alert('Developer says: Ekhono Kaj Kora Hoi Nai');
+});
+
+let addMoneyCard = document.getElementById('addMoney-card');
+addMoneyCard.addEventListener('click', function (event) {
+    event.preventDefault();
+    alert('Developer says: Ekhono Kaj Kora Hoi Nai');
+});
+
+let transferMoneyCard = document.getElementById('transferMoney-card');
+transferMoneyCard.addEventListener('click', function (event) {
+    event.preventDefault();
+    alert('Developer says: Ekhono Kaj Kora Hoi Nai');
+});
+
+let insuranceCard = document.getElementById('insurance-card');
+insuranceCard.addEventListener('click', function (event) {
+    event.preventDefault();
+    alert('Developer says: Ekhono Kaj Kora Hoi Nai');
+});
